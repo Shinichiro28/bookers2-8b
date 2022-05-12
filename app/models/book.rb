274e70faf03b60e789d35a9cc8c0194353e7f196
吉田　慎一郎 <shinichiroyoshida@yoshidashinichirounoMacBook-Pro.local>
2022-05-12 10:41:35 +0900

    Initial Commit

---
+++ app/models/book.rb
@@ -0,0 +1,44 @@
+class Book < ApplicationRecord
+
+  has_one_attached :image
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite' #いいね多い順に投稿表示
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+
+  #投稿数を日付別に表示
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #昨日
+  scope :created_two_days_ago, -> { where(created_at: 2.days.ago.all_day) }#2日前
+  scope :created_three_days_ago, -> { where(created_at: 3.days.ago.all_day) }#3日前
+  scope :created_four_days_ago, -> { where(created_at: 4.days.ago.all_day) }#4日前
+  scope :created_five_days_ago, -> { where(created_at: 5.days.ago.all_day) }#5日前
+  scope :created_six_days_ago, -> { where(created_at: 6.days.ago.all_day) }#6日前
+  scope :created_seven_days_ago, -> { where(created_at: 7.days.ago.all_day) }#7日前
+  #投稿数を週別に表示
+  scope :created_this_week, -> { where(created_at: Time.zone.now.all_week) } #今週
+  scope :created_last_week, -> { where(created_at: 1.week.ago.all_week) }#先週
+
+
+    def favorited_by?(user)
+      favorites.exists?(user_id: user.id)
+    end
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+
+end
